64e20731ba340f9cd008f5fc5dd47dcace306a7c
DrSausage00 <lichtenwaldkirill77@gmail.com>
2026-09-03 13:44:35 +0200

    sending events to kafka again

--- ingestion/main.py
+++ ingestion/main.py
@@ -10,41 +10,35 @@ from parsers.csv_parser import parse_machine_a
 from parsers.json_parser import parse_machine_b
 from parsers.pipe_parser import parse_machine_c
 
+from producer.kafka_producer import send_event
+
 
 MACHINE_A_COUNT = 10
 MACHINE_B_COUNT = 5
 MACHINE_C_COUNT = 3
 
 
+def _append_machine_events(events, family_count, generator, parser):
+    for i in range(family_count):
+        events.append(parser(generator()))
+
+
 def main():
 
     while True:
-
         events = []
 
-        for _ in range(MACHINE_A_COUNT):
-            events.append(
-                parse_machine_a(
-                    generate_machine_a()
-                )
-            )
-
-        for _ in range(MACHINE_B_COUNT):
-            events.append(
-                parse_machine_b(
-                    generate_machine_b()
-                )
-            )
-
-        for _ in range(MACHINE_C_COUNT):
-            events.append(
-                parse_machine_c(
-                    generate_machine_c()
-                )
-            )
+        for i in range(max(MACHINE_A_COUNT, MACHINE_B_COUNT, MACHINE_C_COUNT)):
+            if i < MACHINE_A_COUNT:
+                events.append(parse_machine_a(generate_machine_a()))
+            if i < MACHINE_B_COUNT:
+                events.append(parse_machine_b(generate_machine_b()))
+            if i < MACHINE_C_COUNT:
+                events.append(parse_machine_c(generate_machine_c()))
 
         for event in events:
             print(json.dumps(asdict(event), indent=2))
+            send_event("machine-events", event)
 
         time.sleep(2)
 
--- ingestion/producer/kafka_producer.py
+++ ingestion/producer/kafka_producer.py
@@ -3,6 +3,8 @@ import os
 
 from dataclasses import asdict
 from kafka import KafkaProducer
+
+
 KAFKA_BOOTSTRAP_SERVERS = os.getenv(
     "KAFKA_BOOTSTRAP_SERVERS", 
     "localhost:9092"
@@ -10,7 +12,7 @@ KAFKA_BOOTSTRAP_SERVERS = os.getenv(
 
 producer = KafkaProducer(
     bootstrap_servers=KAFKA_BOOTSTRAP_SERVERS,
-    key_serializer=lambda k: json.dumps(k).encode("utf-8"),
+    key_serializer=lambda k: k.encode("utf-8"),
     value_serializer=lambda v: json.dumps(v).encode("utf-8")
 )
 
