d5a09e8c3a12482d808b149d85eb777d2de09f25
Lars Burian <lars.burian@gmail.com>
2026-09-09 22:17:52 +0200

    unused tool lul

--- tools/deploy-component.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env bash
-set -euo pipefail
-name="$1"
-tag="${2:-0.1}"
-docker build -t "mes/$name:$tag" "./$name"
-minikube image load "mes/$name:$tag"
-kubectl rollout restart "deployment/$name" -n mes
-kubectl rollout status "deployment/$name" -n mes
