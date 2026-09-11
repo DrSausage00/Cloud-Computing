08402bb9f997ebd98d8f50e914f2a4ad1d0c8d31
CharlotteKittel <charlottekittel02@web.de>
2026-09-11 10:00:26 +0200

    Revert "Handle streaming query failures gracefully"
    This reverts commit a84d5ed4403126b505c231162b90042e7e4ce311.

--- stream-processing/streaming_job.py
+++ stream-processing/streaming_job.py
@@ -187,15 +187,3 @@ status_query = (status_stream.writeStream
 
 # wartet auf die Beendigung eines der beiden Streaming-Jobs
 spark.streams.awaitAnyTermination()
-
-while True:
-    try:
-        spark.streams.awaitAnyTermination(timeout=10_000)
-    except Exception as exc:
-        print(f"Eine Query ist gestorben: {exc}")
-    finally:
-        spark.streams.resetTerminated()
-
-    if not spark.streams.active:
-        print("Alle Queries sind beendet. Beende SparkSession.")
-        break
